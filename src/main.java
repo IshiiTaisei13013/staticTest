49452672a15187096ef1a71754000d5f9ad92ec3
IshiiTaisei13013 <Taisei_Ishii@hulft.com>
2021-07-08 15:52:24 +0900

    static 練習

--- src/main.java
+++ src/main.java
@@ -1,6 +1,23 @@
+import jdk.dynalink.beans.StaticClass;
+
 public class main {
     public static void main(String[] args){
         System.out.println(staticTest.staticField);
+        System.out.println(staticTest.staticMethod());
+
+        staticTest.staticField = "Japan";
+        System.out.println(staticTest.staticField);
+
+        staticTest test = new staticTest();
+        System.out.println(test.staticField);
+        System.out.println(test.staticMethod());
+        System.out.println(test.instanceMethod());
+
+        test.instanceField = "Murata";
+        System.out.println(test.instanceMethod());
+
+        test.instanceField = "Hi";
+        System.out.println(test.instanceMethod());
 
     }
 }

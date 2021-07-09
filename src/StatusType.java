9ff40957a63d0afba8141ebe53ccc0db6a68408d
IshiiTaisei13013 <Taisei_Ishii@hulft.com>
2021-07-09 15:32:05 +0900

    [update] interfaceまで進めた

---
+++ src/StatusType.java
@@ -0,0 +1,3 @@
+public enum StatusType {
+    POISON,PARALYSIS,DEATH
+};
--- src/main.java
+++ src/main.java
@@ -2,22 +2,36 @@ import jdk.dynalink.beans.StaticClass;
 
 public class main {
     public static void main(String[] args){
-        System.out.println(staticTest.staticField);
-        System.out.println(staticTest.staticMethod());
+//        System.out.println(staticTest.staticField);
+//        System.out.println(staticTest.staticMethod());
+//
+//        staticTest.staticField = "Japan";
+//        System.out.println(staticTest.staticField);
+//
+//        staticTest test = new staticTest();
+//        System.out.println(test.staticField);
+//        System.out.println(test.staticMethod());
+//        System.out.println(test.instanceMethod());
+//
+//        test.instanceField = "Murata";
+//        System.out.println(test.instanceMethod());
+//
+//        test.instanceField = "Hi";
+//        System.out.println(test.instanceMethod());
+        Object obj = new FooService("Hello");
+        Object obj2 = new FooService("Hello");
 
-        staticTest.staticField = "Japan";
-        System.out.println(staticTest.staticField);
+        System.out.println(obj == obj2);
+        System.out.println(obj instanceof FooService);
+        System.out.println(obj instanceof AbstractBaseService);
+        System.out.println(obj instanceof BaseService);
+        System.out.println(obj instanceof Integer);
 
-        staticTest test = new staticTest();
-        System.out.println(test.staticField);
-        System.out.println(test.staticMethod());
-        System.out.println(test.instanceMethod());
+        if (obj instanceof FooService){
+            FooService service = (FooService) obj;
+            System.out.println(service.say());
+        }
 
-        test.instanceField = "Murata";
-        System.out.println(test.instanceMethod());
-
-        test.instanceField = "Hi";
-        System.out.println(test.instanceMethod());
 
     }
 }
